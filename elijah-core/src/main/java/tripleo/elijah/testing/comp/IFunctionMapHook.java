f15f1e98c52fbfe7e7e7a4af2d46764aac5f5b11
olu <olu@dec17>
2026-06-30 20:08:11 -0400

    redo/cherry-pick(aeed6164): September + Sense stuff.

---
+++ elijah-core/src/main/java/tripleo/elijah/testing/comp/IFunctionMapHook.java
@@ -0,0 +1,13 @@
+package tripleo.elijah.testing.comp;
+
+//
+//import tripleo.elijah.lang.i.*;
+//import tripleo.elijah.stages.gen_fn.EvaFunction;
+//
+//import java.util.Collection;
+//
+//public interface IFunctionMapHook {
+//	boolean matches(FunctionDef aFunctionDef);
+//
+//	void apply(Collection<EvaFunction> aGeneratedFunctions);
+//}
